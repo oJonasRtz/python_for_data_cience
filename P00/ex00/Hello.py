5cf32fcc81d057d120e437a4f5d5dce78e0324f1
Jonas <jonasper19@gmail.com>
2026-06-05 22:44:17 -0300

    ex00-ok

---
+++ P00/ex00/Hello.py
@@ -0,0 +1,26 @@
+
+def draw_values(*args):
+	for arg in args:
+		print(arg)
+	
+
+def hello():
+	#Original objects from the subject
+	ft_list = ["Hello", "tata!"]
+	ft_tuple = ("Hello", "toto!")
+	ft_set = {"Hello", "tutu!"}
+	ft_dict = {"Hello" : "titi!"}
+
+	# change the original objects
+	ft_list[1] = "World!"
+	ft_tuple = ("Hello", "Brazil!")
+	ft_set.remove("tutu!")
+	ft_set.add("Sao Paulo!")
+	ft_dict["Hello"] = "42SaoPaulo!"
+
+	#print changed objects
+	draw_values(ft_list, ft_tuple, ft_set, ft_dict)
+
+
+if __name__ == "__main__":
+	hello()
